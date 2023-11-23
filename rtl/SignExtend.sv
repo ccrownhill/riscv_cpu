120e85d9f44aabb529d64515970a0ae956a3d150
Orlan <orlan.forshaw22@ic.ac.uk>
2023-11-23 14:16:13 +0000

    Some minor changes to the SignExtend.sv module as there were errors

--- rtl/SignExtend.sv
+++ rtl/SignExtend.sv
@@ -6,14 +6,13 @@ module SignExtend (
 	output logic [31:0] ImmOp
 );	
 
-// 00 = 12 bit, 01 = 12 bit, 10 = 12 bit, 11 = 20 bit
 always_comb begin
 	case (ImmSrc)
 		2'b00: ImmOp = {{20{Instr31_7[31]}}, Instr31_7[31:20]};
-		2'b01: ImmOp = {{20{Instr31_7[31]}}, Instr31_7[31:25], Instr31_7[11:4]}
-		2'b10: ImmOp = {{20{Instr31_7[31]}}, Instr31_7[31], Instr31_7[7], Instr31_7[30:25], Instr31_7[11:8]}
-		2'b11: ImmOp = {{12{Instr31_7[31]}}, Instr31_7[19:12], Instr31_7[20], Instr31_7[30:21]}
+		2'b01: ImmOp = {{20{Instr31_7[31]}}, Instr31_7[31:25], Instr31_7[11:7]}
+		2'b10: ImmOp = {{19{Instr31_7[31]}}, Instr31_7[31], Instr31_7[7], Instr31_7[30:25], Instr31_7[11:8], 1'b0}
+		2'b11: ImmOp = {{11{Instr31_7[31]}}, Instr31_7[31], Instr31_7[19:12], Instr31_7[20], Instr31_7[30:21], 1'b0}
 	endcase
 end
-
+// should always output a sign extended 32 bit ImmOp
 endmodule
