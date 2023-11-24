d0a4e3071516e6879997362ee02e258a7a24250c
Orlan <orlan.forshaw22@ic.ac.uk>
2023-11-24 11:29:00 +0000

    made a script for formatting

---
+++ utilscripts/TabRemove.py
@@ -0,0 +1,34 @@
+#!/opt/homebrew/bin/python3
+
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Usage: python TabRemove.py <file_path>,<file_path> ...")
+    sys.exit(1)
+for i in range(1,len(sys.argv),1):
+	file_path = sys.argv[i]  
+
+	try:
+		out = ""
+		with open(file_path, 'r') as file:
+			content = file.readlines()
+		for line in content:
+			line = line.replace('\t', '    ')
+			line = line.rstrip()
+			stripped = line.lstrip()
+			count = (len(line)-len(stripped))//4
+			print(count)
+			modified_content = "  " * count + stripped 
+			out += modified_content + "\n"
+		
+
+
+		with open(file_path, 'w') as file:
+			file.write(out)
+
+		print("Tabs/4 spaces replaced with two spaces in the file:", file_path)
+
+	except FileNotFoundError:
+		print("File not found:", file_path)
+		
