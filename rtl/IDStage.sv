2f54986a3c3f305646a48d6926436cbaf053f968
Orlan <orlan.forshaw22@ic.ac.uk>
2023-11-30 11:37:53 +0000

    small changes in IDStage.sv

--- rtl/IDStage.sv
+++ rtl/IDStage.sv
@@ -13,6 +13,9 @@ module IDStage (
   input logic [4:0]   writeRegAddr_i, // write address from WB stage
   input logic [31:0]  WD3_i, // write data from WB stage
 
+  output logic [4:0] rs1_o
+  output logic [4:0] rs2_o
+
 	output logic        RegWrite_o,
 	output logic        ALUsrc_o,
 	output logic [1:0]  WriteSrc_o,
@@ -126,7 +129,9 @@ ID_EXReg ID_EXReg (
   .funct3_o (funct3_o)
 
   // hazard
-  .rs1D(rs1_i)
-  .rs2D(rs2_i)
+  .rs1_i(rs1_i)
+  .rs2_i(rs2_i)
+  .rs1_o(rs1_o)
+  .rs2_o(rs2_o)
 );
 endmodule
